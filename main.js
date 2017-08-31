244f493bc286f4c679e97a7334b9fa7587d91219
Ankit Rai <raiankit953@gmail.com>
2017-08-31 20:56:05 +0530

    Grid layout. Designing complete.

---
+++ main.js
@@ -0,0 +1,17 @@
+function hook(target){
+    return document.getElementById(target);
+}
+// get elemenets from dom
+const one = hook(one),
+    two = hook(two),
+    three = hook(three),
+    four = hook(four),
+    five = hook(five),
+    six = hook(six),
+    seven = hook(seven),
+    eight = hook(eight),
+    nine = hook(nine),
+    zero = hook(zero),
+    plus = hook(plus),
+    subt = hook(subt)
+// add event listeners
